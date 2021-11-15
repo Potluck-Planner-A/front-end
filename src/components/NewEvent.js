3efee33ac47bc71eab748c8783df7b5897313327
Bryce Slade <bryceslade@gmail.com>
2021-11-15 11:39:18 -0700

    installed dependancies

---
+++ src/components/NewEvent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewEvent = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NewEvent
---
+++ src/components/ViewEvent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewEvent = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ViewEvent
