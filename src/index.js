c769e7cb8ad72c3c1c934e31baa4ece20f3e843d
david fletcher <davidfletcheraaron@gmail.com>
2021-11-15 14:50:32 -0800

    only pushing index

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Header />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
