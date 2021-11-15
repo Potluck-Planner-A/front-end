95a70bebbc6eec694773c13a737c8dee711ae99a
Bryce Slade <bryceslade@gmail.com>
2021-11-15 12:00:33 -0700

    structured App.js and created PrivateRoute component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { Route } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+
+
     </div>
   );
 }
