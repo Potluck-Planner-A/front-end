9a9ceab8bdbfb6c354c79510f8829edf4d2e3f86
david fletcher <davidfletcheraaron@gmail.com>
2021-11-15 15:31:55 -0800

    bringing fixed the switch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-// import { Route } from "react-router-dom";
+import Home from './components/Home'
+import NewEvent from './components/NewEvent'
+import { Switch, Route } from "react-router-dom";
 // import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
   return (
+    <>
     <div className="App">
       <Header/>
 
-
+    <Switch>
+      <Route path='/' component={Home}/>
+      <Route path='/newevent' component={NewEvent}/>
+    </Switch>
     </div>
+    
+
+      <p> 
+        hello world
+      </p>
+
+    </>
   );
 }
 
