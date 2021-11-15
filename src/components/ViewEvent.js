266a98adbe88d8bffc04b3b6cab2b46ef2b423d6
Amethyst Williams <zmithic@gmail.com>
2021-11-15 15:38:39 -0600

    Imported axios; set states; created get request from dummy API; added useEffect to setEvents when page loads

--- src/components/ViewEvent.js
+++ src/components/ViewEvent.js
@@ -1,11 +1,44 @@
-import React from 'react'
+// Import dependencies
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
+// Here's where the styling will go
+
+// Until proper stuff is set up to pull from I'll be using PokeAPI
+
+// Exported component
 const ViewEvent = () => {
+    //----------------  Setting States  ----------------//
+    // Initial event array (starts empty)
+    const initialEvents = [];
+    // Slice of state for events
+    const [events, setEvents] = useState(initialEvents);
+
+    //----------------  Creating Helpers  ----------------//
+    // Grab events from API (currently grabbing from PokeAPI)
+    const getEvents = () => {
+        axios.get('https://pokeapi.co/api/v2/pokemon/')
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.error(`Ruh roh! ${err}`);
+            })
+    };
+
+    //----------------  Side Effects  ----------------//
+    // Acquire events
+    useEffect(() => {
+        getEvents();
+      }, []);
+
+    // Render content
     return (
         <div>
-            
         </div>
     )
-}
+};
 
-export default ViewEvent
+export default ViewEvent;
+
+// Worked on by Amethyst :>
