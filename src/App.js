34e6d8d8be6023e661df9373fdba288984dea226
david fletcher <davidfletcheraaron@gmail.com>
2021-11-15 16:21:21 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home'
-import NewEvent from './components/NewEvent'
 import { Switch, Route } from "react-router-dom";
+import './App.css';
+
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import NewEvent from './components/NewEvent';
+import Register from './components/Register';
 // import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Header/>
+      <div className="App">
+        <Header/>
 
-    <Switch>
-      <Route path='/' component={Home}/>
-      <Route path='/newevent' component={NewEvent}/>
-    </Switch>
-    </div>
+        <Switch>
+          <Route path='/newevent' component={NewEvent}/>
+          <Route path='/register' component={Register}/>
+          <Route path='/login' component={Login}/>
+          <Route exact path='/logout' component={Logout}/>
+          <Route path='/' component={Home}/>
+        </Switch>
+      </div>
     
-
       <p> 
         hello world
       </p>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header from './components/Header';
+// import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Header />
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
