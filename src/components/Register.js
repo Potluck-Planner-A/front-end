28144b27dd46d819b8feff0cf1479fb1cf8aa548
Bryce Slade <bryceslade@gmail.com>
2021-11-17 09:53:56 -0700

    Cleaned up register and Login component

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Grid, Paper, Avatar, TextField } from '@material-ui/core';
+import { Grid, Paper, Avatar, TextField, Button } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 
 const initialState = {
     username: '',
     password: '',
+    email:''
 }
 
 const Register = () => {
@@ -22,57 +23,57 @@ const Register = () => {
         event.preventDefault();
     }
 
-    const paperStyle = {padding: 20, height: '70vh', width: 280, margin: '5rem auto'}
-    const avatarStyle = {backgroundColor:'#626262'}
+    const paperStyle = {padding: 20, height: '70vh', width: 280, margin: '6rem auto'}
+    const avatarStyle = {backgroundColor:'#626262', width: '4rem', height: '4rem'}
+    const textboxStyle = {margin: '0.7rem 0'}
+    const headerStyle = {margin: '2rem 0', fontFamily: 'Outfit, sans-serif'}
+    const iconStyle = {fontSize: '3rem'}
+    const buttonStyle = {backgroundColor: '#a9a9ff', fontFamily: 'Outfit, sans-serif', margin: '0.8rem 0'}
 
     return (
         <Grid>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align='center'>
-                    <Avatar style={avatarStyle} fontSize='large'> <PersonIcon/> </Avatar>
+                    <Avatar style={avatarStyle}> <PersonIcon style={iconStyle}/> </Avatar>
+                    <h2 style={headerStyle}>Create an Account!</h2>
                 </Grid>
 
                 <form onSubmit={handleSubmit}>
 
                     <TextField
-                        label='email'
+                        style={textboxStyle}
+                        label='username'
                         type='text'
-                        placeholder='Enter email'
+                        placeholder='Enter Username'
+                        name='username'
+                        value={state.username}
+                        onChange={handleChanges}
+                        fullWidth
+                    />
+
+                    <TextField
+                        style={textboxStyle}
+                        label='password'
+                        type='password'
+                        placeholder='Enter Password'
+                        name='password'
+                        value={state.password}
+                        onChange={handleChanges}
+                        fullWidth
+                    />
+
+                    <TextField
+                        style={textboxStyle}
+                        label='email (optional)'
+                        type='email'
+                        placeholder='Enter Email'
                         name='email'
-                        // value={}
+                        value={state.email}
                         onChange={handleChanges}
                         fullWidth
                     />
 
-                    <TextField
-                        label=''
-                        type=''
-                        placeholder=''
-                        name=''
-                        // value={}
-                        onChange={handleChanges}
-                        fullWidth
-                    />
-
-                    <TextField
-                        label=''
-                        type=''
-                        placeholder=''
-                        name=''
-                        // value={}
-                        onChange={handleChanges}
-                        fullWidth
-                    />
-
-                    <TextField
-                        label=''
-                        type=''
-                        placeholder=''
-                        name=''
-                        // value={}
-                        onChange={handleChanges}
-                        fullWidth
-                    />
+                    <Button style={buttonStyle} variant='contained' fullWidth>Sign Up</Button>
 
                 </form>
             </Paper>
