4dbb4554fbcf04a9f5f9609e1e2c1e12208dfddf
david fletcher <davidfletcheraaron@gmail.com>
2021-11-17 10:14:34 -0800

    fixed reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,7 @@ import { combineReducers } from "redux";
 import { userReducer as user } from './userReducer';
 import { eventReducer as event } from './eventReducer';
 
-export default combineReducers({ user, event })
+export default combineReducers({ 
+   user, 
+   event 
+});
