b40e9cefb8920384db3cedd8bf03432dc686c808
david fletcher <davidfletcheraaron@gmail.com>
2021-11-16 18:26:10 -0800

    userReducer is ready to take on imports

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,48 @@
-import React from 'react'
+//! this component is for the userLogin ...
+//need imports from actions
 
-function formReducer() {
-   return (
-      <div>
-         
-      </div>
-   )
+export const initialState = {
+   user: {
+      //we may have to do this in a different reducer
+      name: '',
+      username: '',
+      password: '',
+      email: ''
+   },
+   isFetching: false, // this is for the userPage loading
+   error: '',  //this error is if login fails ?
+
 }
 
-export default formReducer
+const userReducer = (state = initialState, action) => {
+   switch(action.type) {
+      case FETCH_START:
+         console.log('fetch is happening')
+         return {
+            ...state,
+            user: [],
+            isFetching: true,
+            error: ''
+         }
+      case FETCH_SUCCESS:
+         console.log('fetch is happening')
+         return {
+            ...state,
+            user: [],
+            isFetching: false,
+            error: ''
+         }
+      case FETCH_FAIL:
+         console.log('fetch is happening')
+         return {
+            ...state,
+            user: [],
+            isFetching: false,
+            error: action.payload
+         }
+      default:
+         return state;
+   }
+}
+
+export default userReducer
