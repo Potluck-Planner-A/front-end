2520c1ca9a8070ede18893bef2719538be4aaa1c
david fletcher <davidfletcheraaron@gmail.com>
2021-11-16 17:54:02 -0800

    started all easy files

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import { userReducer as user } from './userReducer';
+import { eventReducer as event } from './eventReducer';
+
+export default combineReducers({ user, event })
