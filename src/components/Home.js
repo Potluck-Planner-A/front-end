15014fda5015bed62497e033cd6fc21af7202508
Ryan Howard <ryanghoward@protonmail.com>
2021-11-16 19:29:36 -0800

    Finally got Homepage text centered vertically as well. Working on styling it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import "../App.css";
 
 const StyledBackground = styled.div`
   background-image: url("https://253qv1sx4ey389p9wtpp9sj0-wpengine.netdna-ssl.com/wp-content/uploads/2018/11/Dishes_at_Potluck.jpg");
   height: 100vh;
   background-size: cover;
-  margin-top: 64px;
+  margin-top: 4rem;
 `;
 
 const StyledText = styled.div`
-  color: #700207;
-  width: 50vw;
-  margin: 0 auto;
-  background: rgba(255, 255, 255, 0.7);
+  color: #000;
+  width: 60vw;
+  background: rgba(255, 255, 255, 0.8);
   border-radius: 5rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  /* border: 1px solid red; */
+  border: 1px solid red;
 `;
 
 const Home = () => {
   return (
     <StyledBackground>
-      <div className='homepage-text'>
+      <div className='textContainer'>
         <StyledText>
           <h1>Welcome to Potluck Planner!</h1>
           <p>A place to plan and organize a potluck with friends and family</p>
